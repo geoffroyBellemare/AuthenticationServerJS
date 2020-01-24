c3e72ba1aa9334ca393ffbe148a5f6b635ff5215
Geoffroy Bellemare <geoffroybellemare@MacBook-Pro-de-Geoffroy.local>
2020-01-24 12:05:16 +0100

    Authentication with Google OAUTH

---
+++ config/prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+  googleClientID:
+    "168009659198-lprjck56v6m35gcsjh03ptlnjgf7nm1s.apps.googleusercontent.com",
+  googleClientSecret: "geuUXcs-qKLiDgIJNFJWWJVH"
+};
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const passeport = require("passport");
+require("./services/passport");
 const app = express();
+
 const { PORT = 5000 } = process.env;
 
 require("./routes/authRoutes.js")(app);
