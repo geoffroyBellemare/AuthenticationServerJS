f61c3b8a06f640077a4cea2ce993e1fc3ba74894
Geoffroy Bellemare <geoffroybellemare@MacBook-Pro-de-Geoffroy.local>
2020-01-27 11:42:55 +0100

    setting Passeport serialize&encode User

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const passeport = require("passport");
-require("./services/passport");
-const app = express();
-
+const keys = require("./config/keys.js");
 const { PORT = 5000 } = process.env;
+require("./models/User");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const app = express();
+app.use(passeport.initialize());
+app.use(passeport.session());
 
 require("./routes/authRoutes.js")(app);
 
