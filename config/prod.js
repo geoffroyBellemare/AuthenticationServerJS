6a1e8012c781e9cd83d672ecb0bde1fdbb17f153
Geoffroy Bellemare <geoffroybellemare@MacBook-Pro-de-Geoffroy.local>
2020-01-27 14:38:35 +0100

    Prod config file

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,6 @@
 module.exports = {
-  googleClientID:
-    "168009659198-lprjck56v6m35gcsjh03ptlnjgf7nm1s.apps.googleusercontent.com",
-  googleClientSecret: "geuUXcs-qKLiDgIJNFJWWJVH"
+  googleClientID: process.env.googleClientID,
+  googleClientSecret: process.env.googleClientSecret,
+  cookieKey: process.env.cookieKey,
+  mongoURI: process.env.mongoURI
 };
